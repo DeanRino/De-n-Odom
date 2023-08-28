3c4b8ec0144c60de2baf55fe6fae610c42ef0d96
DeanRino <143114232+DeanRino@users.noreply.github.com>
2023-08-28 12:13:33 -0500

    Add files via upload

---
+++ about me/aboutme.py
@@ -0,0 +1,6 @@
+print("About me/n/nme like dog/n/ngouda cheese is best cheese")
+print("")
+print("i like to create digitalart work")
+print("som of my favorite origonal charicters are marty moos, squiggs the squrl, and the unlucky ducky")
+/in
+/in
